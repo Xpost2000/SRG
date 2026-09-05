05d2901e4c0d7881c535b81dab055deb8b382191
Jerry Zhu <xpost2000cod@gmail.com>
2026-09-04 22:37:53 -0700

    fix extra semicolon

--- pcsx-run.bat
+++ pcsx-run.bat
@@ -8,6 +8,6 @@ setlocal
 CALL setvars.bat
 
 if not exist "run-tree\game.bin" (
-  echo Please run build.bat to build the game first.;
-  exit /b 1;
+  echo Please run build.bat to build the game first.
+  exit /b 1
 )
