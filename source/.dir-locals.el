0998c02e5fcb7381382d157419efc999262ca37a
Jerry Zhu <xpost2000cod@gmail.com>
2026-09-23 00:16:22 -0700

    dirlocals for emacs

---
+++ source/.dir-locals.el
@@ -0,0 +1,5 @@
+;; .dir-locals.el
+((c-mode . ((flycheck-gcc-include-path . ("../toolchain-win64/include/libpsn00b"))
+	    (flycheck-clang-include-path . ("../toolchain-win64/include/libpsn00b"))))
+ (c++-mode . ((flycheck-gcc-include-path . ("../toolchain-win64/include/libpsn00b"))
+	      (flycheck-clang-include-path . ("../toolchain-win64/include/libpsn00b")))))
