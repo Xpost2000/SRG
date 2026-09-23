753fd99a5618e671118f4dd5a57d470bcb276bb3
gabe <46883697+Guuzzeji@users.noreply.github.com>
2026-09-22 18:35:34 -0700

    adding in different repo pathing style

--- setvars.bat
+++ setvars.bat
@@ -1,5 +1,5 @@
 @REM
 @REM common include
 @REM
-set PATH=PATH;toolchain-win64\bin;toolchain-win64\cmake-4.4.3-windows-x86_64\bin;toolchain-win64\emulators\
-set PSN00BSDK_LIBS=toolchain-win64\lib\libpsn00b\
+set PATH=%~dp0toolchain-win64\bin;%~dp0toolchain-win64\cmake-4.4.3-windows-x86_64\bin;%~dp0toolchain-win64\emulators;%PATH%
+set PSN00BSDK_LIBS=%~dp0toolchain-win64\lib\libpsn00b
