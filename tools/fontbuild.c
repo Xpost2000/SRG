e082661f0b6b052ffc96f0961f062e2e03097a80
Jerry Zhu <xpost2000cod@gmail.com>
2026-09-26 00:20:16 -0700

    almost

--- tools/fontbuild.c
+++ tools/fontbuild.c
@@ -74,8 +74,10 @@ struct TIM_Image {
 
   // in units of 16 bpp
   // 1 unit here = 4 pixels for 4bpp
-  uint32_t image_width;
-  uint32_t image_height;
+  uint16_t image_width;
+  uint16_t image_height;
+
+  uint8_t* image;
 };
 
 //
@@ -93,8 +95,8 @@ struct SRG_Font {
   // index by ascii char, output is the tile,
   // can find row and col easily from there.
   //
-  // int row = tile/rows;
-  // int col = tile%cols;
+  // int row = tile/columns;
+  // int col = tile%columns;
   //
   uint8_t glyphmap[256];
 
@@ -139,6 +141,41 @@ int get_color_index_from_palette(Color32u8 color)
   return -1;
 }
 
+static void output_tim(TIM_Image* timimage, char* filebasename)
+{
+  char tmp[256];
+  FILE* f;
+
+  snprintf(tmp, 256, "%s.tim", filebasename);
+
+  f = fopen(tmp, "wb+");
+
+#define _fwrite(a,b,c,d) fwrite(d, b, c, a)
+  _fwrite(f, sizeof(uint8_t), 1, &timimage->tag);
+  _fwrite(f, sizeof(uint8_t), 1, &timimage->version);
+  _fwrite(f, sizeof(uint8_t), 1, &timimage->_pad0);
+  _fwrite(f, sizeof(uint8_t), 1, &timimage->_pad1);
+
+  _fwrite(f, sizeof(uint32_t), 1, &timimage->flags);
+
+  _fwrite(f, sizeof(uint32_t), 1, &timimage->clut_length);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->clut_x);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->clut_y);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->clut_width);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->clut_height);
+  _fwrite(f, sizeof(uint16_t) * timimage->clut_width * timimage->clut_height, 1, timimage->clut);
+
+  _fwrite(f, sizeof(uint32_t), 1, &timimage->image_length);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->image_x);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->image_y);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->image_width);
+  _fwrite(f, sizeof(uint16_t), 1, &timimage->image_height);
+  _fwrite(f, sizeof(uint8_t) * timimage->image_width * timimage->image_height, 1, timimage->image);
+#undef _fwrite
+
+  fclose(f);
+}
+
 void insert_palette_color(Color32u8 color)
 {
   int i;
@@ -163,9 +200,19 @@ int main(int argc, char** argv)
   SRG_Font	srgfont;
   uint8_t*	image_pixel_data;
   int		image_width, image_height, image_channels;
+  int           original_rows, original_cols;
+  int           rows, cols;
+
+  //
+  // as 4bpp
+  //
+  int           tim_width, tim_height;
+  int           tim_length;
 
   uint8_t* clut_data;
   uint8_t* tim_image_data;
+
+  memset(&srgfont, 0, sizeof(srgfont));
 
   if (argc != 6) {
     fprintf(stderr, "usage:\n%s <inputimg> <glyphwidth> <glyphheight> <glyphmapfile>  <outputfile>", argv[0]);
@@ -173,17 +220,17 @@ int main(int argc, char** argv)
   }
 
   input_image_filename	= argv[1];
-  output_image_filename = argv[2];
-  glyph_width_str	= argv[3];
-  glyph_height_str	= argv[4];
-  glyph_width		= atoi(glyph_width_str);
-  glyph_height		= atoi(glyph_height_str);
-
+  glyph_width_str	= argv[2];
+  glyph_height_str	= argv[3];
   //
   // The glyph map file is just a string of the characters as they
   // appear within the font file in order 
   //
-  glyph_map_filename    = argv[5];
+  glyph_map_filename    = argv[4];
+  output_image_filename = argv[5];
+
+  glyph_width		= atoi(glyph_width_str);
+  glyph_height		= atoi(glyph_height_str);
 
   printf("input image: \"%s\"\n", input_image_filename);
   printf("output image: \"%s\"\n", output_image_filename);
@@ -196,9 +243,10 @@ int main(int argc, char** argv)
 
   srgfont.image_contents.tag = 0x10;
   srgfont.image_contents.version = 0;
-  srgfont.image_contents.flags = 0x0 | (1 << 4);
+  srgfont.image_contents.flags = 0x0 | (1 << 3);
 
   image_pixel_data = stbi_load(input_image_filename, &image_width, &image_height, &image_channels, 4);
+  printf("source image is %dx%d pixels\n", image_width, image_height);
 
   printf("Building color map / palette.\n");
   {
@@ -221,7 +269,12 @@ int main(int argc, char** argv)
 
     clut_data = (uint8_t*) malloc(sizeof(uint16_t) * 16);
 
-    memset(clut_data, 0, sizeof(uint16_t) * 16);
+    //
+    // set all initial colors transparent
+    //
+    for (i = 0; i < 16; ++i) {
+      clut_data[i] = color32u8_to_5551((Color32u8){0,0,0,0});
+    }
 
     for (i = 0; i < g_palette_used; ++i) {
       clut_data[i] = color32u8_to_5551(g_palette[i]);
@@ -231,29 +284,126 @@ int main(int argc, char** argv)
     // Includes the size of the header as well contents of the clut.
     //
     srgfont.image_contents.clut_length = sizeof(uint32_t) + sizeof(uint16_t) * 4 + sizeof(uint16_t)*16;
+    srgfont.image_contents.clut_width = 16;
+    srgfont.image_contents.clut_height = 1;
+    srgfont.image_contents.clut = (uint16_t*)clut_data;
+    printf("CLUT Length will be: %d\n", srgfont.image_contents.clut_length);
   }
 
   printf("Packing tpage atlas\n");
   {
     int cursor_x = 0;
-    int cursor_y = 0;
-    int rows     = 0;
-    int cols     = 0;
+    rows	 = 1;
+    cols	 = 0;
 
-    int original_rows = image_height / glyph_height;
-    int original_cols = image_width / glyph_width;
-    printf("original image based on given glyph dimensions was rows x cols, %d x %d\n", original_rows, original_cols);
+    original_rows = image_height / glyph_height;
+    original_cols = image_width / glyph_width;
 
+    int tiles_to_pack = original_rows * original_cols;
+
+    printf("original image based on given glyph dimensions was rows x cols, %d x %d = %d tiles to pack\n", original_rows, original_cols, tiles_to_pack);
+    cols = TPAGE_PIXEL_WIDTH / glyph_width; 
+
+    while (tiles_to_pack) {
+      if (cursor_x+glyph_width >= TPAGE_PIXEL_WIDTH) {
+	cursor_x = 0;
+	rows++;
+      } else {
+	cursor_x += glyph_width;
+      }
+
+      --tiles_to_pack;
+    }
+
+    tim_width = cols * glyph_width;
+    tim_height = rows * glyph_height;
+
+    printf("pack estimation says: %d rows, %d (max) columns, %d x %d px 4bpp\n", rows, cols, tim_width, tim_height);
   }
 
   printf("Constructing indexed image\n");
   {
-    
+    int cursor_x      = 0;
+    int cursor_y      = 0;
+    int tiles_to_pack = original_rows * original_cols;
+    int tile_index    = 0;
+
+    tim_length = sizeof(uint8_t) * (cols * glyph_width) * (rows * glyph_height);
+    tim_image_data = (uint8_t*) malloc(tim_length);
+    memset(tim_image_data, 0, sizeof(tim_length));
+    printf("tim image region is: %d bytes\n", tim_length);
+
+    //
+    // same loop as before but we are in packing mode.
+    //
+    for (; tile_index < tiles_to_pack; ++tile_index) {
+
+      //
+      // copy into image data from the original image
+      //
+      {
+	int i;
+	int j;
+
+	int row_offset = tile_index / original_cols;
+	int col_offset = tile_index % original_cols;
+
+	for (i = 0; i < glyph_height; ++i) {
+	  for (j = 0; j < glyph_width; ++j) {
+	    Color32u8 original_pixel;
+	    int       color_index;
+
+	    original_pixel.r = image_pixel_data[((i + (row_offset * glyph_height)) * (image_width * 4) + (j + (col_offset * glyph_width)) * 4) + 0];
+	    original_pixel.g = image_pixel_data[((i + (row_offset * glyph_height)) * (image_width * 4) + (j + (col_offset * glyph_width)) * 4) + 1];
+	    original_pixel.b = image_pixel_data[((i + (row_offset * glyph_height)) * (image_width * 4) + (j + (col_offset * glyph_width)) * 4) + 2];
+	    original_pixel.a = image_pixel_data[((i + (row_offset * glyph_height)) * (image_width * 4) + (j + (col_offset * glyph_width)) * 4) + 3];
+
+#if 0
+	    if (original_pixel.a != 0) {
+	      printf("*");
+	    } else {
+	      printf(" ");
+	    }
+#endif
+
+	    //
+	    // 4 bit index
+	    //
+	    color_index = get_color_index_from_palette(original_pixel) & 0xf;
+
+	    if (((cursor_x + j) % 2) == 0) {
+	      //
+	      // even pixel offset means writing into the other nibble.
+	      //
+	      tim_image_data[(cursor_y + i) * (tim_width/2) + ((cursor_x + j) / 2)] |= color_index;
+	    } else {
+	      tim_image_data[(cursor_y + i) * (tim_width/2) + ((cursor_x + j) / 2)] |= color_index << 4;
+	    }
+	  }
+
+	  printf("\n");
+	}
+      }
+
+      if (cursor_x+glyph_width >= TPAGE_PIXEL_WIDTH) {
+	cursor_x = 0;
+	cursor_y += glyph_height;
+	rows++;
+      } else {
+	cursor_x += glyph_width;
+      }
+    }
   }
 
-  printf("Outputting TIM (for debugging)\n");
+  printf("Outputting TIM (for debugging) (tim data length: %d)\n", tim_length);
   {
-    
+    srgfont.image_contents.image_length = sizeof(uint32_t)*3 + sizeof(uint16_t) * 2 + tim_length;
+    srgfont.image_contents.image_width = tim_width/4;
+    srgfont.image_contents.image_height = tim_height;
+    srgfont.image_contents.image = tim_image_data;
+    printf("%d x %d\n", srgfont.image_contents.image_width, srgfont.image_contents.image_height);
+
+    output_tim(&srgfont.image_contents, output_image_filename);
   }
 
   printf("Outputting SRGFONT\n");
